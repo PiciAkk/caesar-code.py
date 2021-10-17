4f378a76b84a6c5af527d8f2c0d324706a489716
PiciAkk <marci@dbx.hu>
2021-10-17 12:41:27 +0200

    program finally (almost) done

--- decoder.py
+++ decoder.py
@@ -1,9 +1,20 @@
+import string
+
 key = input("Enter key: ")
 text = input("Enter text to decode: ")
 
 alphabet = list(string.ascii_lowercase)
 
 def decodeText(text):
-    for i in range(len(alphabet))
+    decodedText = []
+    for i in list(text):
+        if i == " ":
+            decodedText.append(i)
+        else:
+            alphabetIndex = string.lowercase.index(i)
+            newAlphabetIndex = alphabetIndex - key
+            decodedText.append(alphabet[newAlphabetIndex])
     
-decodeText(text)
+    return "".join(decodedText)
+    
+print(f"Decoded text: {decodeText(text)}")
