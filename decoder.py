210528aa3b7edf23d776056035e29d1767d2863a
PiciAkk <marci@dbx.hu>
2021-10-17 12:33:54 +0200

    first commit

---
+++ decoder.py
@@ -0,0 +1,9 @@
+key = input("Enter key: ")
+text = input("Enter text to decode: ")
+
+alphabet = list(string.ascii_lowercase)
+
+def decodeText(text):
+    for i in range(len(alphabet))
+    
+decodeText(text)
